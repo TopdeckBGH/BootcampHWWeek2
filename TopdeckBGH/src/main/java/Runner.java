a694bd70404742f29d130afd6c7152261198ad8c
Şanver ÜNSAL <sanverunsal@gmail.com>
2022-05-20 13:40:21 +0300

    Week 2 Updated

---
+++ TopdeckBGH/src/main/java/Runner.java
@@ -0,0 +1,13 @@
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.util.concurrent.TimeUnit;
+
+public class Runner {
+    public static void main(String[] args){
+        System.out.println("asd");
+    }
+}
